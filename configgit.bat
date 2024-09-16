d1c9e6fa452a47f4aa14e4003478f20da5c2d474
dezao789 <67484934+dezao789@users.noreply.github.com>
2024-09-16 15:14:50 +0000

    aula 2

--- configgit.bat
+++
@@ -1,3 +0,0 @@
-cmdkey /delete git:https://github.com
-git config --global user.name "André Luiz Duarte Haas"
-git config --global user.email deluizduartehaas@gmail.com
--- index.js
+++
@@ -1,4 +0,0 @@
-function teste(){
-    console.log("Bem Vindo ao JS");
-    
-}
