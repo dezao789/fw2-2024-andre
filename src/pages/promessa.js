1124cc50d3e6448c86bedaf1c18e4801664fd7df
dezao789 <67484934+dezao789@users.noreply.github.com>
2024-10-25 12:15:50 +0000

    atualizando o promessa

--- src/pages/promessa.js
+++ src/pages/promessa.js
@@ -1,17 +1,18 @@
-import Menu from "./modulos/navbar";
+import { useState } from "react";
 import Footer from "./modulos/footer";
+import Menu from "./modulos/navbar";
 import { Container } from "react-bootstrap";
 
 export default function Promessa() {
 const [pao, setpao] = useState("")
     setTimeout(() => {
-        document.title = "Promessa é Divida";
-        setpao("é uma dívida");
+        document.title = "Promessa é dívida";
+        setpao("é dívida");
     }, 4000);
     return <>
         <Menu />
         <Container>
-            Promessa {pao==="" ? "<carregando>":pao}
+            Promessa {pao==="" ? "<img src='' style='width:500px;height:600px;'>" : pao}
         </Container>
         <Footer />
     </>
