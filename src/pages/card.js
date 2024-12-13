cf7ce6c47278015f5eb6e18e22573d886721d5c0
dezao789 <67484934+dezao789@users.noreply.github.com>
2024-12-13 12:14:11 +0000

    aula 13 / 12

--- src/pages/card.js
+++ src/pages/card.js
@@ -1,1 +1,13 @@
-src/pages/card.js
+export default function Card(prop) {
+    return <>
+        <div class="col-sm-3 mb-3 mb-sm-2">
+            <div class="card bg-black text-white">
+                <div class="card-body">
+                    <h5 class="card-title">{prop.titulo}</h5>
+                    <p class="card-text">{prop.descricao}</p>
+                    <a href="#" class="btn btn-primary">{prop.preco}</a>
+                </div>
+            </div>
+        </div>
+    </>
+}
